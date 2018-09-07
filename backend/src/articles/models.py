03e94b0de4550af99c8558bbf279f909ad8879d0
Harrison <harrisonekpobimi@gmail.com>
2018-09-07 14:47:48 +0200

    backend creation

---
+++ backend/src/articles/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
